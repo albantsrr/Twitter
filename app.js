d032cded35ef454b77dbf9fd91406d05d6826f96
albantsrr <albanteissier191@yahoo.fr>
2023-01-19 19:18:40 +0100

    mise en place du dossier controller et des queries

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 const index = require("./routes");
+const errorHandler = require('errorhandler');
 require("./database");
 
 app.set("views", path.join(__dirname, "views"));
@@ -14,5 +15,19 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(index);
+
+console.log(process.env.NODE_ENV);
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(errorHandler())
+}else{
+    app.use((err, req, res, next) => {
+        const code = err.code || 500;
+        res.status(code).json({
+            code: code,
+            message:code === 500 ? null : err.message
+        });
+    })
+}
 
 app.listen(port);
--- database/index.js
+++ database/index.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
+// mongosh "mongodb+srv://cluster0.0pynpbj.mongodb.net/twitter" --apiVersion 1 --root <root>
+// mongodb+srv://root:root@cluster0.rqznbqj.mongodb.net/twitter
+  // .connect("mongodb+srv://root:root@cluster0.rqznbqj.mongodb.net/twitter")
 
 mongoose
-  .connect("mongodb+srv://root:root@cluster0.rqznbqj.mongodb.net/twitter")
+  .connect("mongodb+srv://albantsr:albantsr@cluster0.0pynpbj.mongodb.net/twitter")
   .then(() => console.log("connection db ok"))
   .catch((err) => console.log(err));
